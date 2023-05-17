dffdbbc4e1e561924e43bd004d5aea01a760aa95
clazzor <81418506+clazzor@users.noreply.github.com>
2023-05-17 21:35:54 +0200

    Basic setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask("Kiwi")
+app.config["DEBUG"] = True
+app.config["TESTING"] = True
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+app.run(host='0.0.0.0', port=3310, use_reloader=False)
