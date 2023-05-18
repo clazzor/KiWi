3b7f72bd3073952fa7bce633922239284758b309
clazzor <81418506+clazzor@users.noreply.github.com>
2023-05-18 11:58:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app.config["TESTING"] = True
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!!!!</h1>'
+    return '<h1>Hello, World!</h1>'
 
 app.run(host='0.0.0.0', port=3310, use_reloader=False)
